df982d190683245f777d8389a91deccedcb406ef
bertol26 <bertolfranco26@gmail.com>
2020-06-29 14:16:45 -0500

    cambiis

--- HistClinica/HistClinica/wwwroot/js/ActionsPersona.js
+++ HistClinica/HistClinica/wwwroot/js/ActionsPersona.js
@@ -2,19 +2,19 @@
 
     var id = $("#cboemple option:selected").val();
     if (id == 109) {
-        $("#esp1").hide();
-        $("#esp2").hide();
-        $("#ruc1").hide();
-        $("#ruc2").hide();
-        $("#col1").hide();
-        $("#col2").hide();
-    } else {
         $("#esp1").show();
         $("#esp2").show();
         $("#ruc1").show();
         $("#ruc2").show();
         $("#col1").show();
         $("#col2").show();
+    } else {
+        $("#esp1").hide();
+        $("#esp2").hide();
+        $("#ruc1").hide();
+        $("#ruc2").hide();
+        $("#col1").hide();
+        $("#col2").hide();
     }
 
 });
